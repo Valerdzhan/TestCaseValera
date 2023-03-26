004bcc98cce302c9ad236e1bb1e6dc28d2f7f7b8
Valeriy Ivanov <valerdzhan@gmail.com>
2023-03-27 01:16:10 +0500

    Удалил плагин

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,10 @@ import App from "./App.vue"
 import router from "./router"
 import { createPinia } from "pinia"
 import "@/assets/css/tailwind.css"
-import testPlugin from "./plugins/testPlugin"
 
 const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
-app.use(testPlugin)
 
 app.mount("#app")
