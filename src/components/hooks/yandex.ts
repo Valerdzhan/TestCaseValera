5455d20bc08ad35b986b3b07f8efde004aa92301
Valeriy Ivanov <valerdzhan@gmail.com>
2023-03-26 22:10:46 +0500

    save

--- src/components/hooks/yandex.ts
+++ src/components/hooks/yandex.ts
@@ -4,21 +4,7 @@ import type { LngLat } from "@yandex/ymaps3-types"
 export default function useYandex(options: YandexType) {
 	const { el, coordinate, zoom, markerCoordinate } = options
 
-	const init = async () =>
-		new Promise((resolve) => {
-			if (typeof ymaps3 !== "undefined") {
-				return ymaps3.ready.then(() => resolve(ymaps3))
-			}
-
-			const script = document.createElement("script")
-			script.src = `https://api-maps.yandex.ru/3.0/?apikey=${process.env.VUE_APP_YANDEX_KEY}&lang=ru_RU`
-			script.onload = () => resolve
-
-			document.body.appendChild(script)
-		})
-
 	const initYandexMap = async () => {
-		await init()
 		await ymaps3.ready
 		const { YMapZoomControl, YMapGeolocationControl } = await ymaps3.import(
 			"@yandex/ymaps3-controls@0.0.1"
@@ -29,7 +15,7 @@ export default function useYandex(options: YandexType) {
 
 		const map = new ymaps3.YMap(el as HTMLElement, {
 			location: {
-				center: coordinate as LngLat,
+				center: [66.08308214579333, 76.66800609527584],
 				zoom: zoom,
 			},
 		})
@@ -43,7 +29,7 @@ export default function useYandex(options: YandexType) {
 			),
 			map.addChild(
 				new YMapDefaultMarker({
-					coordinates: markerCoordinate as LngLat,
+					coordinates: [66.08308214579333, 76.66800609527584],
 				})
 			)
 	}
