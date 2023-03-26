5c96f225d60601c592960bca4abcc3db0f4facb8
Valeriy Ivanov <valerdzhan@gmail.com>
2023-03-26 21:40:41 +0500

    save 6

--- src/components/hooks/yandex.ts
+++ src/components/hooks/yandex.ts
@@ -4,34 +4,48 @@ import type { LngLat } from "@yandex/ymaps3-types"
 export default function useYandex(options: YandexType) {
 	const { el, coordinate, zoom } = options
 
-	// const init = async () =>
-	// 	new Promise((resolve) => {
-	// 		if (typeof ymaps3 !== "undefined") {
-	// 			return ymaps3.ready.then(() => resolve(ymaps3))
-	// 		}
+	const init = async () =>
+		new Promise((resolve) => {
+			if (typeof ymaps3 !== "undefined") {
+				return ymaps3.ready.then(() => resolve(ymaps3))
+			}
 
-	// 		const script = document.createElement("script")
-	// 		script.src = `https://api-maps.yandex.ru/3.0/?apikey=${process.env.VUE_APP_YANDEX_KEY}&lang=ru_RU`
-	// 		script.onload = () => resolve
+			const script = document.createElement("script")
+			script.src = `https://api-maps.yandex.ru//?apikey=${process.env.VUE_APP_YANDEX_KEY}&lang=ru_RU`
+			script.onload = () => resolve
 
-	// 		document.body.appendChild(script)
-	// 	})
+			document.body.appendChild(script)
+		})
 
 	const setYMap = async () => {
-		// await init()
-		// const ydiv: HTMLElement = document.createElement("div")
-		// ydiv.style.width = "400px"
-		// ydiv.style.height = "400px"
-		// el?.appendChild(ydiv)
-		const map = new ymaps3.YMap(
-			document.getElementById("YMapsID") as HTMLElement,
-			{
-				location: {
-					center: [35, 55] as LngLat,
-					zoom: 12,
-				},
-			}
+		await init()
+		await ymaps3.ready
+		const { YMapZoomControl, YMapGeolocationControl } = await ymaps3.import(
+			"@yandex/ymaps3-controls@0.0.1"
 		)
+		const { YMapDefaultMarker } = await ymaps3.import(
+			"@yandex/ymaps3-markers@0.0.1"
+		)
+
+		const map = new ymaps3.YMap(el as HTMLElement, {
+			location: {
+				center: coordinate as LngLat,
+				zoom: zoom,
+			},
+		})
+
+		map.addChild(new ymaps3.YMapDefaultSchemeLayer({ theme: "dark" })),
+			map.addChild(new ymaps3.YMapDefaultFeaturesLayer({})),
+			map.addChild(
+				new ymaps3.YMapControls({ position: "right" })
+					.addChild(new YMapZoomControl({}))
+					.addChild(new YMapGeolocationControl({}))
+			),
+			map.addChild(
+				new YMapDefaultMarker({
+					coordinates: coordinate as LngLat,
+				})
+			)
 	}
 
 	return { setYMap }
